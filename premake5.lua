f8f65eb0139839a471cba4624ed0ed43e6b5f173
Cvelth <cvelth.mail@gmail.com>
2021-01-11 11:38:27 +0200

    Add a premake based generator build system

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+local templated = require "script/premake/template"
+templated.third_party "third_party"
+
+templated.language = "C++"
+templated.cppdialect = "C++17"
+templated.manifest_path = ".manifest"
+templated.vpaths = {
+	{ ["precompiled"] = "**/precompiled/**.h*" },
+	{ ["precompiled/source"] = "**/precompiled/**.c*" },
+	{ ["include/detail"] = "include/detail/**.h*" },
+	{ ["source/include/detail"] = "source/detail/**.h*" },
+	{ ["source/include"] = "source/**.h*" },
+	{ ["source/detail"] = "source/detail/**.c*" },
+	{ ["source"] = "source/**.c*" },
+	{ ["include"] = "include/**.h*" }
+}
+
+templated.workspace "vkma"
+
+templated.project "generator"
+    templated.kind "ConsoleApp"
+    files "vulkan_hpp_generator_fork/VulkanHppGenerator.*pp"
+    targetdir "bin/%{cfg.system}_%{cfg.buildcfg}"
+	depends "tinyxml2"
+
+	defines {
+		"VK_SPEC=\"" .. _MAIN_SCRIPT_DIR .. "/vma_xml_generator/output/vma.xml\"",
+		"VULKAN_HPP_FILE=\"" .. _MAIN_SCRIPT_DIR .. "/output/vkma.hpp\""
+	}
